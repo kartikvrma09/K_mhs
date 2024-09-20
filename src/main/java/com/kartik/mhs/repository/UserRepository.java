7ac956a5b0ca06cc70ac6617bd280093697fd400
Kartik <kartivrma09@gmail.com>
2024-09-20 10:29:06 +0530

    first commit

---
+++ src/main/java/com/kartik/mhs/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.kartik.mhs.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.kartik.mhs.model.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    com.kartik.mhs.model.User findByUsername(String username);
+    com.kartik.mhs.model.User findByEmail(String email);
+}
